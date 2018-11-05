d5b5abc272cc796ae27298cec63b95bc2db7b1f5
JooYooGit <jimmy.zhuyu@gmail.com>
2018-11-05 03:31:05 +0100

    added section bg

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,12 @@
 <template>
 <div>
-  <v-container>
-    <v-layout  wrap>
-      <v-flex xs12>
+  
+    <v-layout wrap>
+
+      <v-flex class="cyan darken-2 white--text">
         <header>
-          <nav id="scrolling-nav" v-scroll-spy-link class="navbar navbar-expand-lg">
+          <!-- TODO: scroll dots-->
+          <!-- <nav id="scrolling-nav" v-scroll-spy-link class="navbar navbar-expand-lg">
             <ul class="ml-auto">
               <li class="nav-item">
                 <a class="nav-link btn-circle" href="#about">about</a>
@@ -22,72 +24,90 @@
                 <a class="nav-link btn-circle" href="#timeline"></a>
               </li>
             </ul>
-          </nav>
-          <div >
+          </nav> -->
+
+          <div>
             <h1 class="text-xs-center ">Welcome to Scrolling Nav</h1>
             <p class="text-xs-center ">A landing page template freshly redesigned for Bootstrap 4</p>
           </div>
+
         </header>
       </v-flex>
 
-      <v-flex>
+      <v-container>
         <v-layout >
-          <section id="about">
-            <h2>About this page</h2>
-            <p class="md-lg-left">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>  
-          </section>
+          <v-flex>
+            <section id="section-1">
+              <h2>Section 1</h2>
+              <p>This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>  
+            </section>
+          </v-flex>
         </v-layout>
-      </v-flex>
+      </v-container>
+      
 
-      <v-flex mb-4>
-        <section id="services" class="section hero is-primary is-fullheight bg-light">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8 mx-auto">
-                <h2>Services we offer</h2>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
-              </div>
-            </div>
-          </div>
-        </section>
-      </v-flex>
+        <v-layout class="grey lighten-4">
+          <v-container>
+            <v-flex>
+              <section id="section-2">
+                <h2>Section 2</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+              </section>
+            </v-flex>
+          </v-container>
+        </v-layout>
 
-      <v-flex>
-        <section id="contact" class="section hero is-primary is-fullheight">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8 mx-auto">
-                <h2>Contact us</h2>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
-              </div>
-            </div>
-          </div>
-        </section>
-      </v-flex>
+       <v-container>
+        <v-layout >
+          <v-flex>
+            <section id="section-3">
+              <h2>Section 3</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+            </section>
+          </v-flex>
+        </v-layout>
+      </v-container>
 
-      <v-flex>
-        <section id="test" class="section hero is-primary is-fullheight">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-8 mx-auto">
-						<h2>test us</h2>
-						<p class="lead">test test testjfkjdklfjkdls;jakl;jfdsa</p>
-					</div>
-				</div>
-			</div>
-		</section>
-
-      </v-flex>
+      <v-layout class="grey lighten-4">
+        <v-container>
+          <v-flex>
+            <section id="section-4">
+              <h2>Section 4</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+            </section>
+          </v-flex>
+        </v-container>
+      </v-layout>
 
       
+      <v-container>
+        <v-layout>
+          <v-flex>
+            <section id="section-5">
+              <h2>Section 5</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+            </section>
+          </v-flex>
+        </v-layout>
+      </v-container>
+        
+        
+        
+        
+        
+        
+        
+        
+      
+      
     </v-layout>
-  </v-container>
+ 
 
-    <!-- Footer -->
-   <v-footer dark height="auto">
+  <!-- Footer -->
+   <v-footer dark height="auto" >
     <v-card class="flex" flat tile>
-      <v-card-title class="teal">
-        <strong class="subheading">Get connected with us on social networks!</strong>
+      <v-card-title class="grey darken-4">
+        <strong class="subheading"> &copy;2018 — <strong>ZhuYu</strong></strong>
         <v-spacer></v-spacer>
         <v-btn v-for="icon in icons"
           :key="ion"
@@ -98,13 +118,9 @@
           <v-icon size="24px">{{ icon }}</v-icon>
         </v-btn>
       </v-card-title>
-
-      <v-card-actions class="grey darken-3 justify-center">
-        &copy;2018 — <strong>Vuetify</strong>
-      </v-card-actions>
     </v-card>
   </v-footer>
-</div>
+  </div>
 </template>
 
 <script>
