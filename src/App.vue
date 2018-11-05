e0e7ad986f03fa1b9596492cc82c24e1f75e4f3f
JooYooGit <jimmy.zhuyu@gmail.com>
2018-11-05 02:38:04 +0100

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,78 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+
+    <v-container>
+      <v-layout row wrap>
+         <v-flex xs12>
+              <v-btn flat small>HOME</v-btn>
+              <v-btn flat small color="primary">Project</v-btn>
+              <v-btn flat small color="error">Blog</v-btn>
+              <v-btn flat small disabled>wait...</v-btn>
+          </v-flex>
+      </v-layout>
+    </v-container>
+
 
     <v-content>
       <HelloWorld/>
     </v-content>
+
+     <!-- <v-card height="55px">
+     <v-bottom-nav
+      :active.sync="bottomNav"
+      :color="color"
+      :value="true"
+      absolute
+      shift>
+      <v-btn dark>
+        <span>Home</span>
+        <v-icon>home</v-icon>
+      </v-btn>
+
+      <v-btn dark>
+        <span>Projects</span>
+        <v-icon>apps</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+  </v-card> -->
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   },
-  data () {
+  data() {
     return {
-      //
+      bottomNav: 3
+    };
+  },
+
+  computed: {
+    color() {
+      switch (this.bottomNav) {
+        case 0:
+          return "blue-grey";
+        case 1:
+          return "teal";
+        case 2:
+          return "brown";
+        case 3:
+          return "indigo";
+      }
     }
   }
-}
+};
 </script>
+
+<style>
+.menu-btn {
+  padding-left: 1px;
+  padding-right: 1px;
+}
+</style>
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,147 +1,189 @@
 <template>
+<div>
   <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
+    <v-layout  wrap>
       <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
+        <header>
+          <nav id="scrolling-nav" v-scroll-spy-link class="navbar navbar-expand-lg">
+            <ul class="ml-auto">
+              <li class="nav-item">
+                <a class="nav-link btn-circle" href="#about">about</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link btn-circle" href="#services"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link btn-circle" href="#contact"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link btn-circle" href="#test"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link btn-circle" href="#timeline"></a>
+              </li>
+            </ul>
+          </nav>
+          <div >
+            <h1 class="text-xs-center ">Welcome to Scrolling Nav</h1>
+            <p class="text-xs-center ">A landing page template freshly redesigned for Bootstrap 4</p>
+          </div>
+        </header>
+      </v-flex>
+
+      <v-flex>
+        <v-layout >
+          <section id="about">
+            <h2>About this page</h2>
+            <p class="md-lg-left">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>  
+          </section>
+        </v-layout>
       </v-flex>
 
       <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
+        <section id="services" class="section hero is-primary is-fullheight bg-light">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <h2>Services we offer</h2>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </v-flex>
 
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
+      <v-flex>
+        <section id="contact" class="section hero is-primary is-fullheight">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <h2>Contact us</h2>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </v-flex>
 
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
+      <v-flex>
+        <section id="test" class="section hero is-primary is-fullheight">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 mx-auto">
+						<h2>test us</h2>
+						<p class="lead">test test testjfkjdklfjkdls;jakl;jfdsa</p>
+					</div>
+				</div>
+			</div>
+		</section>
 
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
       </v-flex>
 
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+      
     </v-layout>
   </v-container>
+
+    <!-- Footer -->
+   <v-footer dark height="auto">
+    <v-card class="flex" flat tile>
+      <v-card-title class="teal">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+        <v-spacer></v-spacer>
+        <v-btn v-for="icon in icons"
+          :key="ion"
+          class="mx-3"
+          dark
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;2018 — <strong>Vuetify</strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
+export default {
+  data: () => ({
+   icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram'
       ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
-
-      ]
-    })
-  }
+   
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer"
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/layout/pre-defined"
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
+      }
+    ]
+  })
+};
 </script>
 
 <style>
+header {
+  padding: 0px 0 240px;
+}
 
+@media (min-width: 992px) {
+  header {
+    padding: 240px 0 240px;
+  }
+}
+
+.navbar-brand {
+  margin-left: 10px;
+}
+
+.nav-item {
+  margin-top: 12px;
+  margin-left: 15px;
+}
+
+#navbarText {
+  margin-left: 160px;
+}
+
+#scrolling-nav {
+  margin-right: 40px;
+  color: black;
+}
+
+a {
+  color: black;
+}
+
+section {
+  padding: 150px 0;
+}
+
+ul {
+  list-style-type: none;
+}
+
+.btn-circle {
+  width: 11px;
+  height: 8px;
+  padding: 5px 5px;
+  border-radius: 15px;
+  font-size: 10px;
+  line-height: 0;
+  border: 2px solid black;
+}
 </style>
