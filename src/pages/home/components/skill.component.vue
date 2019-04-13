ff244ed46603605f3cb7df3c8bf3a2aa3ee349c6
JooYoo <jooyoo@outlook.com>
2019-04-13 06:37:09 +0200

    updated skills

--- src/pages/home/components/skill.component.vue
+++ src/pages/home/components/skill.component.vue
@@ -3,24 +3,55 @@
     <div id="skill-title" class="display-2 font-weight-thin">Skill</div>
     <v-layout row wrap>
       <v-flex xs12>
-        
         <v-card class="rounded-card">
           <v-card-text class="overflow-hidden py-0">
-            <v-layout row align-content-center class="horiz-scroll" >
+            <v-layout row align-content-center class="horiz-scroll">
 
-             
-
-              <v-flex px-1 pb-2 >
+              <v-flex px-1 pb-2>
                 <div class="pos-relative">
                   <v-list three-line>
-                    <v-subheader>Web</v-subheader>
-                    <v-list-tile v-for="item in frontend" :key="item.id">
+                    <v-subheader>Coding</v-subheader>
+                    <v-list-tile v-for="item in coding" :key="item.id">
                       <v-list-tile-action>
                         <img :src="item.icon" width="25px">
                       </v-list-tile-action>
                       <v-list-tile-content>
                         <v-list-tile-title>{{item.name}}</v-list-tile-title>
                         <v-progress-linear :color="item.color" v-model="item.progress"></v-progress-linear>
+                      </v-list-tile-content>
+                    </v-list-tile>
+                  </v-list>
+                </div>
+              </v-flex>
+
+              <v-flex px-1 pb-2>
+                <div class="pos-relative">
+                  <v-list three-line>
+                    <v-subheader>Web</v-subheader>
+                    <v-list-tile v-for="item in web" :key="item.id">
+                      <v-list-tile-action>
+                        <img :src="item.icon" width="25px">
+                      </v-list-tile-action>
+                      <v-list-tile-content>
+                        <v-list-tile-title>{{item.name}}</v-list-tile-title>
+                        <v-progress-linear :color="item.color" v-model="item.progress"></v-progress-linear>
+                      </v-list-tile-content>
+                    </v-list-tile>
+                  </v-list>
+                </div>
+              </v-flex>
+
+              <v-flex px-1 pb-2>
+                <div class="pos-relative">
+                  <v-list three-line>
+                    <v-subheader>Computer Vision</v-subheader>
+                    <v-list-tile v-for="item in computerVision" :key="item.id">
+                      <v-list-tile-action>
+                        <img :src="item.icon" width="25px">
+                      </v-list-tile-action>
+                      <v-list-tile-content>
+                        <v-list-tile-title>{{item.name}}</v-list-tile-title>
+                        <v-progress-linear :color="item.progressColor" v-model="item.progress"></v-progress-linear>
                       </v-list-tile-content>
                     </v-list-tile>
                   </v-list>
@@ -44,77 +75,123 @@
                 </div>
               </v-flex>
 
-              <v-flex px-1 pb-2>
-                <div class="pos-relative">
-                  <v-list three-line>
-                    <v-subheader>Computer Vision</v-subheader>
-                    <v-list-tile v-for="item in others" :key="item.id">
-                      <v-list-tile-action>
-                        <img :src="item.icon" width="25px">
-                      </v-list-tile-action>
-                      <v-list-tile-content>
-                        <v-list-tile-title>{{item.name}}</v-list-tile-title>
-                        <v-progress-linear :color="item.progressColor" v-model="item.progress"></v-progress-linear>
-                      </v-list-tile-content>
-                    </v-list-tile>
-                  </v-list>
-                </div>
-              </v-flex>
-              
             </v-layout>
           </v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
   </v-container>
-
 </template>
 
 <script>
 export default {
   data: () => ({
-    frontend: [
+    coding: [
+      {
+        name: "Java",
+        progress: 10,
+        icon: "https://cdn.svgporn.com/logos/java.svg",
+        color: "indigo darken-2"
+      },
+      {
+        name: "C#",
+        progress: 60,
+        icon: "https://www.flamingoajans.com/assets/vendors/devicon/icons/csharp/csharp-original.svg",
+        color: "indigo darken-1"
+      },
+      {
+        name: "Javascript",
+        progress: 50,
+        icon: "https://cdn.svgporn.com/logos/javascript.svg",
+        color: "indigo darken-1"
+      },
+      {
+        name: "HTML5",
+        progress: 80,
+        icon: "https://cdn.svgporn.com/logos/html-5.svg",
+        color: "indigo darken-1"
+      },
+      {
+        name: "CSS",
+        progress: 60,
+        icon: "https://cdn.svgporn.com/logos/css-3.svg",
+        color: "indigo darken-1"
+      },
+      {
+        name: "Git",
+        progress: 80,
+        icon: "https://cdn.svgporn.com/logos/git-icon.svg",
+        color: "indigo darken-1"
+      }
+    ],
+    web: [
       {
         name: "Angular",
         progress: 40,
         icon: "https://cdn.svgporn.com/logos/angular-icon.svg",
-        color: "blue darken-1"
-      },
-      {
-        name: "ionic",
-        progress: 60,
-        icon: "https://cdn.svgporn.com/logos/ionic.svg",
-        color: "blue darken-1"
+        color: "red darken-1"
       },
       {
         name: "Vuejs",
         progress: 30,
         icon: "https://cdn.svgporn.com/logos/vue.svg",
-        color: "blue darken-1"
-      },
-      {
-        name: "Bootstrap",
-        progress: 80,
-        icon: "https://cdn.svgporn.com/logos/bootstrap.svg",
-        color: "blue darken-1"
-      },
-      {
-        name: "jQuery",
-        progress: 30,
-        icon: "https://cdn.svgporn.com/logos/jquery.svg",
-        color: "blue darken-1"
-      },
-      {
-        name: "Vuetify",
-        progress: 30,
-        icon: "https://cdn.svgporn.com/logos/vuetifyjs.svg",
-        color: "blue darken-1"
+        color: "red darken-1"
       },
       {
         name: "ASP.NET Core",
         progress: 40,
         icon: "https://cdn.svgporn.com/logos/dotnet.svg",
-        color: "blue darken-1"
+        color: "red darken-1"
+      },
+      {
+        name: "ionic",
+        progress: 60,
+        icon: "https://cdn.svgporn.com/logos/ionic.svg",
+        color: "red darken-1"
+      },
+      {
+        name: "Bootstrap",
+        progress: 80,
+        icon: "https://cdn.svgporn.com/logos/bootstrap.svg",
+        color: "red darken-1"
+      },
+      {
+        name: "jQuery",
+        progress: 30,
+        icon: "https://cdn.svgporn.com/logos/jquery.svg",
+        color: "red darken-1"
+      },
+      {
+        name: "Vuetify",
+        progress: 30,
+        icon: "https://cdn.svgporn.com/logos/vuetifyjs.svg",
+        color: "red darken-1"
+      }
+    ],
+    computerVision: [
+      {
+        name: "Unity 3D",
+        progress: 50,
+        icon: "https://cdn.svgporn.com/logos/unity.svg",
+        progressColor: "blue darken-1"
+      },
+      {
+        name: "ARCore",
+        progress: 60,
+        icon: "https://blog.novoda.com/content/images/2018/05/arcore.png",
+        progressColor: "blue darken-1"
+      },
+      {
+        name: "Tensorflow",
+        progress: 5,
+        icon: "https://cdn.svgporn.com/logos/tensorflow.svg",
+        progressColor: "blue darken-1"
+      },
+      {
+        name: "OpenCV",
+        progress: 2,
+        icon: "https://cdn.svgporn.com/logos/opencv.svg",
+        progressColor: "blue darken-1"
       }
     ],
     design: [
@@ -130,7 +207,7 @@ export default {
         icon: "https://image.flaticon.com/icons/svg/552/552224.svg",
         progressColor: "teal lighten-1"
       },
-       {
+      {
         name: "Adobe Photoshop",
         progress: 20,
         icon: "https://image.flaticon.com/icons/svg/552/552220.svg",
@@ -148,40 +225,13 @@ export default {
         icon: "https://image.flaticon.com/icons/svg/552/552462.svg",
         progressColor: "teal lighten-1"
       },
-       {
+      {
         name: "iBooks Author",
         progress: 70,
         icon: "https://image.flaticon.com/icons/svg/831/831307.svg",
         progressColor: "teal lighten-1"
-      },
-    ],
-    others: [
-     
-      {
-        name: "Unity 3D",
-        progress: 50,
-        icon: "https://cdn.svgporn.com/logos/unity.svg",
-        progressColor: "orange darken-4"
-      },
-      {
-        name: "ARCore",
-        progress: 60,
-        icon: "https://blog.novoda.com/content/images/2018/05/arcore.png",
-        progressColor: "orange darken-4"
-      },
-      {
-        name: "Tensorflow",
-        progress: 5,
-        icon: "https://cdn.svgporn.com/logos/tensorflow.svg",
-        progressColor: "orange darken-4"
-      },
-      {
-        name: "OpenCV",
-        progress: 2,
-        icon: "https://cdn.svgporn.com/logos/opencv.svg",
-        progressColor: "orange darken-4"
       }
-    ]
+    ],
   })
 };
 </script>
@@ -193,7 +243,7 @@ export default {
 }
 #skill-title {
   margin-top: 40px;
- 
+
   margin-bottom: 50px;
 }
 
@@ -216,6 +266,4 @@ export default {
   overflow-y: hidden;
   overflow-x: scroll;
 }
-
-
 </style>
